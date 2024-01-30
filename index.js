c5b007df80a1528f728781dc2caa7d87e2947ad0
Carballosa <350096@cognizant.com>
2024-01-30 17:07:54 +0100

    Use json-server module

---
+++ index.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server')
+const cors = require('cors');
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(cors());
+server.use(middlewares)
+server.use(router)
+
+server.listen(process.env.port || 3000, () => {
+  console.log('JSON Server is running')
+})
